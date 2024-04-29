b3ffe11f051d1d122c8ed43fff7cdebf889e73f0
bcoulibal <coulou789@gmail.com>
2024-04-29 13:26:56 +0000

    fix

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::Write};
 
 pub fn open_or_create(file: &str, content: &str) {
-    let mut file = File::options().create(true).open(file).unwrap();
+    let mut file = File::create(file).expect("Failed to create file");
     file.write_all(content.as_bytes()).expect("Failed to write to file");
 }
 
