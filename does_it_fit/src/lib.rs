3ad9afc37d8e59700c82800069f3f248f0e86b54
bcoulibal <coulou789@gmail.com>
2024-05-01 18:26:42 +0000

    fix: volume check

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -37,20 +37,20 @@ pub fn volume_fit(
 ) -> bool {
     match objects {
         areas_volumes::GeometricalVolumes::Cube => {
-            cube_volume(a) * times <= cube_volume(rectangle_area(x, y))
+            cube_volume(a) * times <= (y*x*z)
         }
         areas_volumes::GeometricalVolumes::Sphere => {
-            sphere_volume(a) * times as f64 <= cube_volume(rectangle_area(x, y)) as f64
+            sphere_volume(a) * times as f64 <= (y*x*z) as f64
         }
         areas_volumes::GeometricalVolumes::Pyramid => {
             triangular_pyramid_volume(a as f64, b) * times as f64
-                <= cube_volume(rectangle_area(x, y)) as f64
+                <= (y*x*z) as f64
         }
         areas_volumes::GeometricalVolumes::Parallelepiped => {
-            parallelepiped_volume(a, b, c) * times <= cube_volume(rectangle_area(x, y))
+            parallelepiped_volume(a, b, c) * times <= (y*x*z)
         }
         areas_volumes::GeometricalVolumes::Cone => {
-            cone_volume(a, b) * times as f64 <= cube_volume(rectangle_area(x, y)) as f64
+            cone_volume(a, b) * times as f64 <= (y*x*z) as f64
         }
     }
 }
