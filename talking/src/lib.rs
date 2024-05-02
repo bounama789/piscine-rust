0523c946bc37a2bcb6e6fa7b0514da53e34cfc06
bcoulibal <coulou789@gmail.com>
2024-05-02 13:32:02 +0000

    fix

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn talking(text: &str) -> &str {
     if text.trim().is_empty() {
         "Just say something!"
-    } else if text == text.to_uppercase() {
-        if text.ends_with('?') {
+    } else if text == text.trim().to_uppercase() {
+        if text.trim().ends_with('?') {
             "Quiet, I am thinking!"
         } else {
             "There is no need to yell, calm down!"
         }
-    } else if text.ends_with('?') {
+    } else if text.trim().ends_with('?') {
         "Sure."
     } else {
         "Interesting"
