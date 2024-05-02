cbc219352969dd6319b15ee6e45213ef36b399de
bcoulibal <coulou789@gmail.com>
2024-05-02 13:49:34 +0000

    logic_number

---
+++ logic_number/src/lib.rs
@@ -0,0 +1,6 @@
+pub fn number_logic(num: u32) -> bool {
+    let num_str = num.to_string();
+    let power = num_str.len() as u32;
+    let sum: u32 = num_str.chars().map(|c| c.to_digit(10).unwrap().pow(power)).sum();
+    num == sum
+}
