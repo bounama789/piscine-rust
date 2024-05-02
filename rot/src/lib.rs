5acf8f83de2763f3ac24c5aef3ae368af8f26b1b
bcoulibal <coulou789@gmail.com>
2024-05-02 14:25:52 +0000

    fix

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -3,7 +3,7 @@ pub fn rotate(input: &str, key: i8) -> String {
     for c in input.chars() {
         if c.is_ascii_alphabetic() {
             let base = if c.is_ascii_lowercase() { b'a' } else { b'A' };
-            let offset = (c as u8 - base + key as u8) % 26;
+            let offset = (c as u8).wrapping_sub(base).wrapping_add(key as u8) % 26;
             result.push((base + offset) as char);
         } else {
             result.push(c);
