fdee8cf4fbde496cf791d94680bad93445eb756a
Bounama Coulibaly <coulou789@gmail.com>
2024-05-02 02:12:17 +0000

    mobs

---
+++ mobs/src/mobs.rs
@@ -0,0 +1,73 @@
+pub mod boss;
+pub mod member;
+
+use boss::Boss;
+use member::{Member, Role};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Mob {
+    pub name: String,
+    pub boss: Boss,
+    pub members: Vec<Member>,
+    pub cities: Vec<(String, u8)>,
+    pub wealth: u32,
+}
+
+impl Mob {
+    pub fn recruit(&mut self, name: String, age: u8) {
+        self.members.push(Member::new(&name, Role::Associate, age));
+    }
+
+    pub fn attack(&mut self, other: &mut Mob) {
+        let self_power = self
+            .members
+            .iter()
+            .map(|m| m.role.clone() as u32)
+            .sum::<u32>();
+        let other_power = other
+            .members
+            .iter()
+            .map(|m| m.role.clone() as u32)
+            .sum::<u32>();
+
+        if self_power == other_power {
+            self.members.pop();
+        } else {
+            let mut winner: Option<&mut Mob> = None;
+            let mut loser: Option<&mut Mob> = None;
+
+            if self_power < other_power {
+                winner = Some(self);
+                loser = Some(other);
+            } else {
+                winner = Some(other);
+                loser = Some(self);
+            };
+            if let Some(ref mut l) = loser {
+                l.members.pop();
+
+                if l.members.is_empty() {
+                    if let Some(w) = winner {
+                        w.cities.append(&mut l.cities);
+                        w.wealth += l.wealth;
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn steal(&mut self, target: &mut Mob, amount: u32) {
+        let actual_amount = amount.min(target.wealth);
+        target.wealth -= actual_amount;
+        self.wealth += actual_amount;
+    }
+
+    pub fn conquer_city(&mut self, mobs: &[Mob], city_name: String, value: u8) {
+        if !mobs
+            .iter()
+            .any(|m| m.cities.iter().any(|(name, _)| name == &city_name))
+        {
+            self.cities.push((city_name, value));
+        }
+    }
+}
