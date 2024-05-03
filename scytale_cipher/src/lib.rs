e926401ebecb98703e61117d92a472b136c9e82b
Bounama Coulibaly <coulou789@gmail.com>
2024-05-03 18:00:58 +0000

    fix: SCYTALE_CIPHER

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,13 +1,20 @@
-pub fn scytale_cipher(message: String, size: u32) -> String {
-    let size = size as usize;
-    if size == 0 {
-        return message;
+pub fn scytale_cipher(message: String, i: u32) -> String {
+    let len = message.len();
+    let padded_message = if len % i as usize == 0 {
+        message.chars().collect::<Vec<char>>()
+    } else {
+        (message + &" ".repeat(i as usize - len % i as usize))
+            .chars()
+            .collect::<Vec<char>>()
+    };
+    let len = padded_message.len();
+    let mut result = String::new();
+    for j in 0..i {
+        let mut k = j;
+        while k < len as u32 {
+            result.push(padded_message[k as usize]);
+            k += i;
+        }
     }
-
-    let mut rows = vec![String::new(); size];
-    for (index, character) in message.chars().enumerate() {
-        rows[index % size].push(character);
-    }
-
-    rows.concat()
+    result.trim().to_string()
 }
