f5656929f42348bf7c9732d520eb4ca24a4d51cc
Bounama Coulibaly <coulou789@gmail.com>
2024-05-03 17:10:37 +0000

    fix

--- rgb_match/src/lib.rs
+++ rgb_match/src/lib.rs
@@ -1,31 +1,62 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
 }
-
 impl Color {
     pub fn swap(mut self, first: u8, second: u8) -> Color {
-        let mut x = first;
-        let mut y = second;
-        if self.r == x {
-            self.r = y;
-            (x, y) = (y, x);
-        }
-
-        if self.g == x {
-            self.g = y;
-            (x, y) = (y, x);
-        }
-        if self.b == x {
-            self.b = y;
-            (x, y) = (y, x);
-        }
-        if self.a == x {
-            self.a = y;
+        match (first, second) {
+            (f, s) if f == self.r && s == self.g => {
+                self.r = self.g;
+                self.g = f;
+            }
+            (f, s) if f == self.r && s == self.b => {
+                self.r = self.b;
+                self.b = f;
+            }
+            (f, s) if f == self.r && s == self.a => {
+                self.r = self.a;
+                self.a = f;
+            }
+            (f, s) if f == self.g && s == self.r => {
+                self.g = self.r;
+                self.r = f;
+            }
+            (f, s) if f == self.g && s == self.b => {
+                self.g = self.b;
+                self.b = f;
+            }
+            (f, s) if f == self.g && s == self.a => {
+                self.g = self.a;
+                self.a = f;
+            }
+            (f, s) if f == self.b && s == self.r => {
+                self.b = self.r;
+                self.r = f;
+            }
+            (f, s) if f == self.b && s == self.g => {
+                self.b = self.g;
+                self.g = f;
+            }
+            (f, s) if f == self.b && s == self.a => {
+                self.b = self.a;
+                self.a = f;
+            }
+            (f, s) if f == self.a && s == self.r => {
+                self.a = self.r;
+                self.r = f;
+            }
+            (f, s) if f == self.a && s == self.b => {
+                self.a = self.b;
+                self.b = f;
+            }
+            (f, s) if f == self.a && s == self.g => {
+                self.a = self.g;
+                self.g = f;
+            }
+            _ => {}
         }
         self
     }
