2d32e680e4e4e35da160b46e13da3c4d66f2d0e2
Bounama Coulibaly <coulou789@gmail.com>
2024-05-03 17:26:23 +0000

    fix

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -9,10 +9,10 @@ pub fn pig_latin(text: &str) -> String {
         } else {
             let mut consonant_cluster = String::new();
             let mut rest = String::new();
-            let mut chars = word.chars();
+            let mut chars = word.char_indices();
             let mut found_vowel = false;
 
-            while let Some(c) = chars.next() {
+            while let Some((idx,c)) = chars.next() {
                 if vowels.contains(c) {
                     rest.push(c);
                     found_vowel = true;
@@ -20,8 +20,8 @@ pub fn pig_latin(text: &str) -> String {
                 } else {
                     consonant_cluster.push(c);
                     // Special case for 'qu'
-                    if c == 'q' {
-                        if let Some(next_c) = chars.next() {
+                    if c == 'q' && idx != 0 {
+                        if let Some((idx, next_c)) = chars.next() {
                             if next_c == 'u' {
                                 consonant_cluster.push(next_c);
                             } else {
@@ -33,7 +33,7 @@ pub fn pig_latin(text: &str) -> String {
             }
 
             if found_vowel {
-                rest.extend(chars);
+                rest.extend(chars.map(|(_,c)| c));
             }
             result.push_str(&format!("{}{}ay ", rest, consonant_cluster));
         }
