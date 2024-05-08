165f63265db055f9ca79a20798254c0e33ae0f35
Bounama Coulibaly <coulou789@gmail.com>
2024-05-08 19:04:13 +0000

    ref_cell

---
+++ ref_cell/src/lib.rs
@@ -0,0 +1,49 @@
+pub mod messenger;
+
+pub use messenger::{Logger, Tracker};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
+
+pub struct Worker {
+    pub track_value: Rc<usize>,
+    pub mapped_messages: RefCell<HashMap<String, String>>,
+    pub all_messages: RefCell<Vec<String>>,
+}
+
+impl Worker {
+    pub fn new(value: usize) -> Self {
+        Worker {
+            track_value: Rc::new(value),
+            mapped_messages: RefCell::new(HashMap::new()),
+            all_messages: RefCell::new(Vec::new()),
+        }
+    }
+}
+
+impl Logger for Worker {
+    fn warning(& self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Warning".to_string(), msg.to_string());
+        self.all_messages
+            .borrow_mut()
+            .push(format!("Warning: {}", msg));
+    }
+
+    fn info(& self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Info".to_string(), msg.to_string());
+        self.all_messages
+            .borrow_mut()
+            .push(format!("Info: {}", msg));
+    }
+
+    fn error(& self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Error".to_string(), msg.to_string());
+        self.all_messages
+            .borrow_mut()
+            .push(format!("Error: {}", msg));
+    }
+}
---
+++ ref_cell/src/messenger.rs
@@ -0,0 +1,45 @@
+use std::{cell::RefCell, rc::Rc};
+
+// Define the Logger trait
+pub trait Logger {
+    fn warning(&self, msg: &str);
+    fn info(&self, msg: &str);
+    fn error(&self, msg: &str);
+}
+
+// Define the Tracker structure
+pub struct Tracker<'z, L: Logger> {
+    logger: &'z L,
+    pub value: RefCell<usize>,
+    max: u32,
+}
+impl<'z, L: Logger> Tracker<'z, L> {
+    pub fn new(logger: &'z L, max: u32) -> Self {
+        Tracker {
+            logger,
+            value: RefCell::new(0),
+            max,
+        }
+    }
+
+    pub fn set_value(&self, value: &Rc<usize>) {
+        *self.value.borrow_mut() = Rc::strong_count(value);
+        let percentage = (*self.value.borrow() * 100) as u32 / self.max as u32;
+        if percentage >= 100 {
+            self.logger.error("Error: you are over your quota!");
+        } else if percentage >= 70 {
+            self.logger.warning(&format!(
+                "Warning: you have used up over {}% of your quota! Proceed with precaution",
+                percentage
+            ));
+        }
+    }
+
+    pub fn peek(&self, value: &Rc<usize>) {
+        let percentage = (Rc::strong_count(value) * 100) as u32 / self.max;
+        self.logger.info(&format!(
+            "Info: you are using up to {}% of your quota",
+            percentage
+        ));
+    }
+}
