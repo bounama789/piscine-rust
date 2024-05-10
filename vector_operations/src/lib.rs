2281c22212c15612802fb3d482b20ade9ac2f07b
Bounama Coulibaly <coulou789@gmail.com>
2024-05-10 19:11:10 +0000

    vector_operations

---
+++ vector_operations/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ThreeDVector<T> {
+    pub i: T,
+    pub j: T,
+    pub k: T,
+}
+
+use std::ops::{Add, Sub};
+
+impl<T: Add<Output = T>> Add for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        ThreeDVector {
+            i: self.i + rhs.i,
+            j: self.j + rhs.j,
+            k: self.k + rhs.k,
+        }
+    }
+}
+impl<T: Sub<Output = T>> Sub for ThreeDVector<T> {
+    type Output = ThreeDVector<T>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        ThreeDVector {
+            i: self.i - rhs.i,
+            j: self.j - rhs.j,
+            k: self.k - rhs.k,
+        }
+    }
+}
