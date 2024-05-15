6e3b9bcd2910ed4cabe17d23aa58ea207e0157a5
Bounama Coulibaly <coulou789@gmail.com>
2024-05-15 21:44:42 +0000

    lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct Person<'s> {
+    pub name: &'s str,
+    pub age: u8,
+}
+
+impl<'s> Person<'s> {
+    pub fn new(name: &str) -> Person {
+        Person { name, age: 0 }
+    }
+}
