f0bc581a3f9a3f753e6744de238324d705545db5
Bounama Coulibaly <coulou789@gmail.com>
2024-05-15 19:07:00 +0000

    matrix_ops

--- matrix/src/ops.rs
+++ matrix/src/ops.rs
@@ -5,18 +5,20 @@ impl<T: Scalar> Add for Matrix<T> {
     type Output = Option<Self>;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let r = self.0.len();
-        let c = if r > 0 {
-            self.0.first().unwrap().len()
-        } else {
-            0
-        };
-        let mut m = Matrix::zero(r, c);
-        for i in 0..r {
-            for j in 0..c {
-                m.0.get_mut(index)
+        let len = self.0.len();
+        let mut m = Vec::new();
+        for i in 0..len {
+            if self.0[i].len() !=rhs.0[i].len()  {
+                return None;
             }
+            let mut r = Vec::new();
+            for j in 0..self.0[i].len() {
+                let n =  self.0[i][j].add(rhs.0[i][j]);
+                r.push(n)
+            }
+            m.push(r);
         };
+        Some(Matrix(m))
     }
 }
 
@@ -24,6 +26,19 @@ impl<T: Scalar> Sub for Matrix<T> {
     type Output = Option<Self>;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        todo!()
+        let len = self.0.len();
+        let mut m = Vec::new();
+        for i in 0..len {
+            if self.0[i].len() !=rhs.0[i].len()  {
+                return None;
+            }
+            let mut r = Vec::new();
+            for j in 0..self.0[i].len() {
+                let n =  self.0[i][j].sub(rhs.0[i][j]);
+                r.push(n)
+            }
+            m.push(r);
+        };
+        Some(Matrix(m))
     }
 }
