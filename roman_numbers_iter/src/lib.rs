dc7cf22efa11eebc0ba7720c124670eb9c450e44
Bounama Coulibaly <coulou789@gmail.com>
2024-05-19 09:51:13 +0000

    refactor: remove unecesary fields

--- roman_numbers_iter/src/lib.rs
+++ roman_numbers_iter/src/lib.rs
@@ -3,24 +3,18 @@ extern crate roman_numbers;
 pub use roman_numbers::{RomanDigit, RomanNumber};
 
 pub struct RomanNumberIter {
-    current: Option<RomanNumber>,
     decimal:u32
 }
 
 impl RomanNumberIter {
     pub fn new(start: RomanNumber) -> Self {
         RomanNumberIter {
-            current: Some(start.clone()),
             decimal: start.to_decimal()
         }
     }
 
-    pub fn next(&mut self) -> Option<RomanNumber> {
-        if let Some(_) = self.current.take() {
-            Some(RomanNumber::from(self.decimal+1))
-        } else {
-            None
-        }
+    pub fn next(&mut self) -> RomanNumber {
+           RomanNumber::from(self.decimal+1)
     }
 }
 
@@ -32,7 +26,8 @@ impl RomanNumberExt for RomanNumber {
     fn next(&mut self) -> Option<RomanNumber> {
         // Create an iterator with self as the starting value
         let mut iter = RomanNumberIter::new(self.clone());
-        iter.next()
+        Some( iter.next())
+       
     }
 }
 
